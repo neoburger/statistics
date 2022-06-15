81d8470782a9b8c82e2dc42b35801d27ffa32e14
dusmart <dusmart@qq.com>
2022-06-15 13:21:10 +0800

    upgrade to neo v3.3.1

--- Statistics.cs
+++ Statistics.cs
@@ -4,14 +4,28 @@ using Neo.Network.P2P.Payloads;
 using Neo.Persistence;
 using Neo.SmartContract;
 using Neo.VM;
-public class Statistics : Plugin, IPersistencePlugin
+using System;
+using System.Linq;
+
+public class Statistics : Plugin
 {
     private static readonly UInt160 BNEO = UInt160.Parse("0x48c40d4666f93408be1bef038b6722404d9a4c2a");
     private static readonly UInt160 TEE = UInt160.Parse("0x82450b644631506b6b7194c4071d0b98d762771f");
     private static readonly UInt160 DAO = UInt160.Parse("0x54806765d451e2b0425072730d527d05fbfa9817");
     private static readonly ulong UNTIL = ulong.TryParse(Environment.GetEnvironmentVariable("UNTIL"), out UNTIL) ? UNTIL : TimeProvider.Current.UtcNow.ToTimestampMS();
     private static readonly string DIR = "data";
-    void IPersistencePlugin.OnPersist(NeoSystem system, Block block, DataCache snapshot, IReadOnlyList<Blockchain.ApplicationExecuted> applicationExecutedList)
+
+    public Statistics()
+    {
+        Blockchain.Committing += OnPersist;
+    }
+
+    public override void Dispose()
+    {
+        Blockchain.Committing -= OnPersist;
+    }
+
+    void OnPersist(NeoSystem system, Block block, DataCache snapshot, IReadOnlyList<Blockchain.ApplicationExecuted> applicationExecutedList)
     {
         Console.Error.WriteLine($"SYNC: {block.Index}: {block.Timestamp} / {UNTIL}");
         if (block.Timestamp > UNTIL)
